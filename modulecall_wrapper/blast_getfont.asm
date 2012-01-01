a085365d2fae1e0386f17408816941b218860c3b
ec429 <ec429@cam.ac.uk>
2012-01-01 20:49:22 +0000

    Fix the getfont() C wrapper

--- modulecall_wrapper/blast_getfont.asm
+++ modulecall_wrapper/blast_getfont.asm
@@ -5,10 +5,11 @@ include "modulecall.asm"
 .blast_getfont
 	ld hl,2
     add hl,sp			; skip over return address on stack
-    ld a,(hl)			; a = fontid
+    ld a,(hl)			; A = fontid
 	
 	ld h,MODULE_ID
 	ld l,modulecall_blast_getfont
 	call MODULECALL
 	
+	ex de,hl			; put the result in HL
 	ret
